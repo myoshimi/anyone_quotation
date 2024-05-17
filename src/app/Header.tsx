2b50d4b5e30d7873fce4113b1bda4a9f81ff34de
YOSHIMI Masato <yoshimi.masato@tis.co.jp>
2024-05-17 22:06:58 +0900

    画面イメージ作成

---
+++ src/app/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Col, Row, Typography } from "antd";
+
+export const Header = () => {
+    return (
+        <header style={{ height: "60px", backgroundColor: "#f0f0f0" }}>
+            <h1> 街の発明家向け見積もりサイト(仮)</h1>
+        </header>
+    );
+};
---
+++ src/lib/appwrite.ts
@@ -0,0 +1,12 @@
+import { Client, Account, Databases } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('664725f300337ad66f5f');
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+
+export { ID } from 'appwrite';
