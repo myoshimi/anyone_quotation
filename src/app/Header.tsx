e2fc07a79f94c607aa2bf23062c44a824329df3a
myoshimi <myoshimi@minilab.biz>
2024-05-18 19:40:34 +0900

    一回消して作り直し

--- src/app/Header.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export const Header = () => {
-    return (
-        <header style={{ height: '60px', backgroundColor: '#f0f0f0' }}>
-            <h1> 街の発明家向け見積もりサイト(仮)</h1>
-        </header>
-    );
-};
